55935a65cd06a333c16034c21eb71439295dc83c
Angel Valkov <angel4o2003@abv.bg>
2026-01-30 19:17:55 +0200

    change

--- components/layout/announcement-bar.tsx
+++ components/layout/announcement-bar.tsx
@@ -2,7 +2,7 @@
 
 export function AnnouncementBar() {
   return (
-    <div className="w-full bg-[#B08F2A] text-white">
+    <div className="w-full bg-[#B08F2A] text-black">
       <div className="mx-auto max-w-screen-2xl px-4 py-2 text-center text-xs font-semibold sm:text-sm">
         Безплатна доставка на поръчки над 49 евро (97.51 лева)
       </div>
